a580836171865adebff078f77fa9adeb1f072d42
Fiqrian <fiqrian.faturahman@gmail.com>
2021-12-02 20:10:29 +0700

    tugas 6 javascript

---
+++ tugas16.js
@@ -0,0 +1,7 @@
+const DeretAngka = () => {
+  Max = Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+  Min = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+  console.log("Nilai Minimal :", Min);
+  console.log("Nilai Maksimal :", Max);
+};
+DeretAngka();
